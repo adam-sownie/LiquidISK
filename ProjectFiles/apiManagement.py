966d8dd5b1fe9029011e21f86120dd80316b3ea1
adam-sownie <sownieadam.@gmail.com>
2015-12-13 13:59:48 -0800

    made path global added listing keys

--- ProjectFiles/apiManagement.py
+++ ProjectFiles/apiManagement.py
@@ -1,6 +1,16 @@
 import sys
 
+apiFile = 'keys.api'
+
 def ListKeys():
+  i = 0
+
+  with open(apiFile) as apiKeys:
+    for key in apiKeys:
+      i += 1
+      print(i)
+      print key
+
   return True
 
 def RemoveKey():
@@ -10,7 +20,7 @@ def AddKey():
   keyID = raw_input('\nKey ID: ')
   verificationCode = raw_input('Verification Code: ')
 
-  apiKeys = open('keys.api', 'a')
+  apiKeys = open(apiFile, 'a')
 
   apiKeys.write(keyID)
   apiKeys.write(',')
